3a339d6ed3fc7fe01d31fdd6174d9d81f5af56a0
CorgiCitrons <eliina.sala@gmail.com>
2020-06-26 07:09:56 +0300

    last tasks 25/06

--- SEF/src/sef/module8/activity/CustomExceptionActivity.java
+++ SEF/src/sef/module8/activity/CustomExceptionActivity.java
@@ -6,21 +6,9 @@ public class CustomExceptionActivity extends Exception{
 
 	//2 - Override getMessage() and provide the following message 
 	// "You are not allowed to use this program"
-	private int validUsers;
-	public CustomExceptionActivity (String message, int validUsers) {
-		super(message);
-		this.validUsers = validUsers;
-	}
-		public CustomExceptionActivity (String message, int validUsers, Throwable cause) {
-			super(message, cause);
-			this.validUsers = validUsers;
-		}
-		public String toString() {
-			return super.toString();
-		}
 		public String getMessage() {
-			return super.getMessage() +"You are not allowed to use this program";
-			
+			System.out.println("You are not allowed to use this program");
+			return "You are not allowed to use this program";			
 		 }
 }
 
