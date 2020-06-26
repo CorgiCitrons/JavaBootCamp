9cc294c312f29865cce7d2869dbbc578d86a7479
CorgiCitrons <eliina.sala@gmail.com>
2020-06-26 11:42:06 +0300

    day 4 _II

--- SEF/src/sef/module9/sample/ListSample.java
+++ SEF/src/sef/module9/sample/ListSample.java
@@ -35,7 +35,11 @@ public class ListSample {
 
 	    
 	    //1 - Add 3 more elements to the list - A, B and C
-	    
+	    // Add an element to the list
+	    list.add("A");
+	    list.add("B");
+	    list.add("C");
+
 	    
 	    
 		//call a method which prints this list
@@ -49,5 +53,9 @@ public class ListSample {
 		//individual elements of a Collection
 		
 		//2 - Use Iterator to iterate over this list.
+		Iterator i = l.iterator();
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
 			}
 }
